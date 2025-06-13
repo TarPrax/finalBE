d168724efb0feed70ceb8f48a3001a5a8f591cef
TarPrax <newtarunsh@gmail.com>
2025-06-13 17:14:15 +0530

    request and feed page

--- Utilis/database.js
+++ Utilis/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://newtarunsh:May9794505375@namastenode.joiqp.mongodb.net/"
+    "mongodb+srv://newtarunsh:May9794505375@namastenode.joiqp.mongodb.net/devTinder"
   );
 };
 
