6e962cd3d79f60ff96033a6456e0082472feaa3d
TarPrax <newtarunsh@gmail.com>
2025-07-04 15:08:59 +0530

    adjusted endpoint

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ const authRouter = require("./src/router/auth");
 const userEdit = require("./src/router/useredit");
 const connection = require("./src/router/connection");
 const authZ = require("./authentication");
+const { endpoint } = require("./src/endpoint");
 
 const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: `${endpoint}`, credentials: true }));
 
 app.use("/", authRouter);
 app.use("/", userEdit);
